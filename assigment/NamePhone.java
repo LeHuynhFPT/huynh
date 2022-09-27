af5cb058e1adc5659fe20b2a844e9cb693c96601
Admin <jftjf7@hgf.com>
2022-09-27 17:48:29 +0700

    bài tập bài 8

---
+++ assigment/NamePhone.java
@@ -0,0 +1,28 @@
+package ss4.BaiTap;
+
+public class NamePhone {
+    private String name;
+    private String number;
+    public NamePhone() {
+        super();
+    }
+    public NamePhone(String name, String number) {
+        super();
+        this.name = name;
+        this.number = number;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getNumber() {
+        return number;
+    }
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+
+}
